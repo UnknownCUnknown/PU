ed9c49dc6b20e000a893477b04d2b712de3e10bb
hcb <jsnjhcb@icloud.com>
2015-04-24 16:52:24 +0800

    加了多线程

--- pu_multi_thread.py
+++ pu_multi_thread.py
@@ -28,111 +28,93 @@ class PU(threading.Thread):
                 print "Yes"
             else:
                 print "NO"
-
-    def login(self, stuid,pas):
+    def login(stuid,pas): 
+        print stuid
         req_url = "http://www.pocketuni.net/"
         request_url = "http://pocketuni.net/index.php?app=home&mod=Public&act=doLogin"
-        school = "南京邮电大学"
+        #school = ""
+        school = "******" #学校中文名
+        #构造头部
         headers = {
             "Host": "pocketuni.net",
             "Referer":"http://www.pocketuni.net/",
             "DNT" : "1",
             "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:36.0) Gecko/20100101 Firefox/36.0",
-            "Referer": "http://202.119.225.34/default_ysdx.aspx",
             "Content-Type": "application/x-www-form-urlencoded"
         }
+        #处理Cookie
         CookieJar = cookielib.CookieJar()
-
         opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(CookieJar))
         try:
             res = opener.open(req_url)
         except Exception:
             return 1
+        #用opener打开页面获取登陆时要post的数据中__hash__一项的值
         p = re.compile("<input type=\"hidden\" name=\"__hash__\" value=\"[^\"]*")
         txt = res.read()
-
         p = p.findall(txt)
         try:
             p = str(p[0])
-    
             s = p.split('value=\"')
             _hash_ = s[1]
         except Exception:
             return 1
+        #print _hash_
+        #构造post数据
         data = {
             "school": school,
-            "sid": "592",
+            "sid": "",#学校id
             "number": stuid,
             "password": pas,
             "login": "登 录",
             "__hash__": _hash_
         }
         post_data = urllib.urlencode(data)
+        #登陆
         req = urllib2.Request(request_url,post_data,headers)
+        sch = 'njupt' #学校简写，例如njupt
+        schurl = 'https://' + sch + '.pocketuni.net' #该校的url
         try:
             opener.open(req)
-            res = opener.open("http://njupt.pocketuni.net/")
+            res = opener.open(schurl)
         except Exception:
             return 1
         txt = res.read()
+        #如果打开该校的pu url有登陆按钮，则表示并未成功登陆，获得失败
         failed = "clogin"
         if failed in txt:
             return 1
+        #构造投票数据的头部
         toupiaopost_headers = {
-            "Host" :"njupt.pocketuni.net",
+            "Host" :schurl,#该校的url
             "DNT" : "1",
             "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:36.0) Gecko/20100101 Firefox/36.0",
             "Content-Type": "application/x-www-form-urlencoded"
         }
-        req_url = "http://njupt.pocketuni.net/index.php?app=event&mod=Front&act=vote"
+        req_url = 'http://' + sch + '.pocketuni.net/index.php?app=event&mod=Front&act=vote'
+        #根据猜测，id应为活动编号，pid因为参赛团队编号 
         toupiao_data = {
-            "id" : "85008",
-            "pid" : "25139"
+            "id" : "",
+            "pid" : ""
         }
         try:
-            toupiaopostdata = urllib.urlencode(toupiao_data)
-            req1 = urllib2.Request(req_url,toupiaopostdata,toupiaopost_headers)
-            opener.open(req1)
-            toupiao_data["pid"] = "25081"
-            toupiaopostdata = urllib.urlencode(toupiao_data)
-            req1 = urllib2.Request(req_url,toupiaopostdata,toupiaopost_headers)
-            opener.open(req1)
-            toupiao_data["pid"] = "25145"
-            toupiaopostdata = urllib.urlencode(toupiao_data)
-            req1 = urllib2.Request(req_url,toupiaopostdata,toupiaopost_headers)
-            opener.open(req1)
-            toupiao_data["pid"] = "25084"
-            toupiaopostdata = urllib.urlencode(toupiao_data)
-            req1 = urllib2.Request(req_url,toupiaopostdata,toupiaopost_headers)
-            opener.open(req1)
-            toupiao_data["pid"] = "25465"
-            toupiaopostdata = urllib.urlencode(toupiao_data)
-            req1 = urllib2.Request(req_url,toupiaopostdata,toupiaopost_headers)
-            opener.open(req1)
-            toupiao_data["id"] = "85031"
-            toupiao_data["pid"] = "25083"
-            toupiaopostdata = urllib.urlencode(toupiao_data)
-            req1 = urllib2.Request(req_url,toupiaopostdata,toupiaopost_headers)
-            opener.open(req1)
-            toupiao_data["pid"] = "25445"
-            toupiaopostdata = urllib.urlencode(toupiao_data)
-            req1 = urllib2.Request(req_url,toupiaopostdata,toupiaopost_headers)
-            opener.open(req1)
-            toupiao_data["pid"] = "25441"
-            toupiaopostdata = urllib.urlencode(toupiao_data)
-            req1 = urllib2.Request(req_url,toupiaopostdata,toupiaopost_headers)
-            opener.open(req1)
-            toupiao_data["pid"] = "25433"
-            toupiaopostdata = urllib.urlencode(toupiao_data)
-            req1 = urllib2.Request(req_url,toupiaopostdata,toupiaopost_headers)
-            opener.open(req1)
-            toupiao_data["pid"] = "25425"
-            toupiaopostdata = urllib.urlencode(toupiao_data)
-            req1 = urllib2.Request(req_url,toupiaopostdata,toupiaopost_headers)
-            opener.open(req1)
+            #要投票的两个活动
+            ids = ["85008","85031"]
+            #要投票的五个团队
+            pids = ["25139","25081","25145","25084","25465","25083","25445","25441","25433","25425"]
+            #投票要post的数据
+            for j in range(0,2):
+                toupiao_data["id"] = ids[j]
+                for i in range(0,6):
+                    toupiao_data["pid"] = pids[j*5+i]
+                    toupiaopostdata = urllib.urlencode(toupiao_data)
+                    req1 = urllib2.Request(req_url,toupiaopostdata,toupiaopost_headers)
+                    opener.open(req1)
+                    #成功结束返回0，异常结束返回1
         except Exception:
             return 1
         return 0
+
 
 conn = MySQLdb.connect(host="localhost", user="***", passwd="****", db="***", charset="utf8")
 mycursor = conn.cursor()
@@ -140,6 +122,6 @@ mycursor.execute("select stu_id from info")
 stu_list = mycursor.fetchall()
 li = []
 for i in range(0, 50):
-    thread = shua()
+    thread = PU()
     thread.start()
     li.append(thread)
